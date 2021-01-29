fd1c42bc774996bc49b689d764bd91d1ccb7fcb3
KenjxJJ <kenjxjj@gmail.com>
2021-01-30 00:48:28 +0300

    Add a tracing product page [#176687106]

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Nagivation from "./components/Navigation";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import TraceProduct from "./pages/TraceProductPage";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
 
         <Switch>
           <Route exact path="/" component={HomePage}/>
+          <Route path="/trace" component={TraceProduct}/>
         </Switch>
 
       </Router>
