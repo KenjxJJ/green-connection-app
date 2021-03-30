904cfa48d410c07ac4846db14b9801fb148f6e49
KenjxJJ <kenjxjj@gmail.com>
2021-03-30 12:57:34 +0300

    Add stylings to the app.

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -5,37 +5,52 @@ const AboutUsPage = () => {
   return (
     <>
       <section className="about-page px-5 py-5">
-      <p className="other-link">&#8592;
-        <Link className="other-link my-2 font-weight-bold" href="/"> Back to Home</Link>
-        </p>
-        <header>
-          <h1 className="display-4 brand"> We are at the best!</h1>
-          <p className="lead font-weight-bolder font-italic">Slogan: Leave everyone happy!</p>
-        </header>
-        <main className="py-3 font-weight-normal">
-          <p>
-            Established in 2020, during the COVID-19 crisis. People globally got
-            restricted to their homes, Warehouse goods got stranded with everything of economy stuck in one place.
-            Problems became to arise when particular products were definately inaccessible to buy or get, in local communities.
+        <div className="w-75 w-lg-50 mx-auto">
+          <p className="other-link">
+            &#8592;
+            <Link className="other-link my-2 font-weight-bold" href="/">
+              {" "}
+              Back to Home
+            </Link>
           </p>
-          <p>
-            {" "}
-            There arose need of transport means for easy delivery of such inaccessible products to
-            people, door to door in the world amidst the lockdown restrictions
-            in various countries. We have served so far over 2,300 homes in so far countries in Europe, United States and Eastern Africa using
-            our online platform connecting our well-serving suppliers, consumers and loaders (transporters).
-            <br />
+          <header>
+            <h1 className="display-4 brand"> We are at the best!</h1>
+            <p className="lead font-weight-bolder font-italic">
+              Slogan: Leave everyone happy!
             </p>
-            <p> Each customer creates an account on this platform, and makes
-            an order and we deliver to their doorsteps.
-          </p>
-        </main>
-        <footer className="py-2">
-          <p>
-            We are the
-            <span className="lead font-weight-bold brand"> CargoTracker</span>.
-          </p>
-        </footer>
+          </header>
+          <main className="py-3 font-weight-normal">
+            <p>
+              Established in 2020, during the COVID-19 crisis. People globally
+              got restricted to their homes, Warehouse goods got stranded with
+              everything of economy stuck in one place. Problems became to arise
+              when particular products were definately inaccessible to buy or
+              get, in local communities.
+            </p>
+            <p>
+              {" "}
+              There arose need of transport means for easy delivery of such
+              inaccessible products to people, door to door in the world amidst
+              the lockdown restrictions in various countries. We have served so
+              far over 2,300 homes in so far countries in Europe, United States
+              and Eastern Africa using our online platform connecting our
+              well-serving suppliers, consumers and loaders (transporters).
+              <br />
+            </p>
+            <p>
+              {" "}
+              Each customer creates an account on this platform, and makes an
+              order and we deliver to their doorsteps.
+            </p>
+          </main>
+          <footer className="py-2">
+            <p>
+              We are the
+              <span className="lead font-weight-bold brand"> CargoTracker</span>
+              .
+            </p>
+          </footer>
+        </div>
       </section>
     </>
   );
--- src/pages/TransactionsPage.jsx
+++ src/pages/TransactionsPage.jsx
@@ -6,19 +6,21 @@ import Orders from "../components/Orders";
 const TransactionsPage = () => {
   return (
     <>
-      <section className="px-4 py-3 transactions-page">
-        <header className="transaction-header lead py-4 my-2">
-          <h1 className="py-2"> All Transactions</h1>
-          <p>
-            Here is the overview of the transactions made on this platform
-            currently.
-          </p>
-        </header>
-        <main>
-          <Orders />
-          <Invoices />
-          <Receipts />
-        </main>
+      <section className="my-0 transactions-page">
+        <div className="mx-auto">
+          <header className="transaction-header mx-n2 lead py-4 px-5 text-center">
+            <h1 className="py-3"> All Transactions</h1>
+            <p>
+              Here is the overview of the transactions made on this platform
+              currently.
+            </p>
+          </header>
+          <main className="px-4 py-3 w-75 mx-auto">
+            <Orders />
+            <Invoices />
+            <Receipts />
+          </main>
+        </div>
       </section>
     </>
   );
