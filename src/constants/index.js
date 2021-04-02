0508f848063ce7be1948501b836119da7838c083
KenjxJJ <kenjxjj@gmail.com>
2021-04-02 09:29:06 +0300

    Add a working backend link[#176687106][#176687116]

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 
 // Link to the backend database
 
-export const API_URL = "http://127.0.0.1:8000/api/products/";
+export const API_URL = "https://banana-pudding-11491.herokuapp.com/api/products/";
